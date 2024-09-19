c61c21570145785601e2c9402161057dd15afbdc
BPplays <andymalbp@gmail.com>
2024-09-19 04:29:16 -0700

    added hosts backup

--- main.go
+++ main.go
@@ -269,11 +269,13 @@ func main() {
 			// Update IPv6 list and reapply template
 			ipv6ListTemplate.Reset()
 			for _, ipv6 := range currentIPv6Addresses {
+				spaces = 0
 				for _, ipv6 := range currentIPv6Addresses {
 					if len(ipv6) > spaces {
 						spaces = len(ipv6)
 					}
 				}
+				spaces += 4
 				ipv6ListTemplate.WriteString(fmt.Sprintf("%s%s%s %s\n", ipv6, strings.Repeat(" ", spaces-len(ipv6)), hostname, hostnameExtra))
 			}
 
